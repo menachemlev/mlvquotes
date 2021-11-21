91a9a9cf712c7724c421dfd8a158f7e8b62520bf
menachemlev12@gmail.com <menachemlev12@gmail.com>
2021-11-21 12:29:18 +0200

    ready for deploy

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,7 @@ app.options(
 app.use('/api/photos', express.static(path.join(__dirname, './photos')));
 app.use(express.static(path.join(__dirname, './../frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, './../frontend/build/index.html'))
 );
 //Route handler
 /////////////////////////////////////////
